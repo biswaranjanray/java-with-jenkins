fb0a6b9bfd9d3f383337346b9383febcb76bbdc4
biswaranjanray <roy.biswa@gmail.com>
2023-08-29 13:03:02 +0530

    first commit

---
+++ src/main/java/com/java/jenkins/JavaWithJenkinsApplication.java
@@ -0,0 +1,13 @@
+package com.java.jenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaWithJenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaWithJenkinsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/java/jenkins/JavaWithJenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.jenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaWithJenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
